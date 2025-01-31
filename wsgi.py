d24f9fd505ef50987b0f918590b6989b049d12b7
jgbb1234 <157638211+jgbb1234@users.noreply.github.com>
2025-01-31 16:54:57 +0000

    Add fixes

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 import click, sys
 from models import db, User, Todo
-from app import app
 from sqlalchemy.exc import IntegrityError
-
+from app import app
 
 
 @app.cli.command("init", help="Creates and initializes the database")
@@ -17,14 +16,67 @@ def initialize():
   print(bob)
   print('database intialized')
 
-@app.cli.command('get-todos')
+
+@app.cli.command("get-user", help="Retrieves a User by username or id")
+@click.argument('key', default='bob')
+def get_user(key):
+  bob = User.query.filter_by(username=key).first()
+  if not bob:
+    bob = User.query.get(int(key))
+    if not bob:
+      print(f'{key} not found!')
+      return
+  print(bob)
+
+
+@app.cli.command("change-email")
 @click.argument('username', default='bob')
-def get_user_todos(username):
+@click.argument('email', default='bob@mail.com')
+def change_email(username, email):
   bob = User.query.filter_by(username=username).first()
   if not bob:
-      print(f'{username} not found!')
-      return
-  print(bob.todos)
+    print(f'{username} not found!')
+    return
+  bob.email = email
+  db.session.add(bob)
+  db.session.commit()
+  print(bob)
+
+
+@app.cli.command('get-users')
+def get_users():
+  users = User.query.all()
+  print(users)
+
+
+@app.cli.command('create-user')
+@click.argument('username', default='rick')
+@click.argument('email', default='rick@mail.com')
+@click.argument('password', default='rickpass')
+def create_user(username, email, password):
+  newuser = User(username, email, password)
+  try:
+    db.session.add(newuser)
+    db.session.commit()
+  except IntegrityError as e:
+    db.session.rollback()
+    print(e.orig)
+    print("Username or email already taken!")
+  else:
+    print(newuser)
+
+
+@app.cli.command('delete-user')
+@click.argument('username', default='bob')
+def delete_user(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print(f'{username} not found!')
+    return
+  db.session.delete(bob)
+  db.session.commit()
+  print(f'{username} deleted')
+
 
 @app.cli.command('add-todo')
 @click.argument('username', default='bob')
@@ -32,12 +84,24 @@ def get_user_todos(username):
 def add_task(username, text):
   bob = User.query.filter_by(username=username).first()
   if not bob:
-      print(f'{username} not found!')
-      return
+    print(f'{username} not found!')
+    return
   new_todo = Todo(text)
   bob.todos.append(new_todo)
   db.session.add(bob)
   db.session.commit()
+  print('Todo added!')
+
+
+@app.cli.command('get-todos')
+@click.argument('username', default='bob')
+def get_user_todos(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print(f'{username} not found!')
+    return
+  print(bob.todos)
+
 
 @click.argument('todo_id', default=1)
 @click.argument('username', default='bob')
@@ -55,67 +119,25 @@ def toggle_todo_command(todo_id, username):
   todo.toggle()
   print(f'{todo.text} is {"done" if todo.done else "not done"}!')
 
-@app.cli.command("get-user", help="Retrieves a User")
+
+@click.argument('category', default='chores')
 @click.argument('username', default='bob')
-def get_user(username):
-  bob = User.query.filter_by(username=username).first()
-  if not bob:
-    print(f'{username} not found!')
+@click.argument('todo_id', default=1)
+@app.cli.command('add-category', help="Adds a category to a todo")
+def add_todo_category_command(
+    category,
+    todo_id,
+    username,
+):
+  user = User.query.filter_by(username=username).first()
+  if not user:
+    print(f'user {username} not found!')
     return
-  print(bob)
 
-@app.cli.command('get-users')
-def get_users():
-  # gets all objects of a model
-  users = User.query.all()
-  print(users)
+  res = user.add_todo_category(todo_id, category)
+  if not res:
+    print(f'{username} has no todo id {todo_id}')
+    return
 
-@app.cli.command("change-email")
-@click.argument('username', default='bob')
-@click.argument('email', default='bob@mail.com')
-def change_email(username, email):
-  bob = User.query.filter_by(username=username).first()
-  if not bob:
-      print(f'{username} not found!')
-      return
-  bob.email = email
-  db.session.add(bob)
-  db.session.commit()
-  print(bob)
-
-@app.cli.command('create-user')
-@click.argument('username', default='rick')
-@click.argument('email', default='rick@mail.com')
-@click.argument('password', default='rickpass')
-def create_user(username, email, password):
-  newuser = User(username, email, password)
-  try:
-    db.session.add(newuser)
-    db.session.commit()
-  except IntegrityError as e:
-    db.session.rollback()
-    print("Username or email already taken!") 
-  else:
-    print(newuser) 
-
-@app.cli.command('delete-user')
-@click.argument('username', default='bob')
-def delete_user(username):
-  bob = User.query.filter_by(username=username).first()
-  if not bob:
-      print(f'{username} not found!')
-      return
-  db.session.delete(bob)
-  db.session.commit()
-  print(f'{username} deleted')
-
-@app.cli.command('delete-user')
-@click.argument('username', default='bob')
-def delete_user(username):
-  bob = User.query.filter_by(username=username).first()
-  if not bob:
-      print(f'{username} not found!')
-      return
-  db.session.delete(bob)
-  db.session.commit()
-  print(f'{username} deleted')
+  todo = Todo.query.get(todo_id)
+  print(todo)
